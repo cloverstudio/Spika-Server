9d5e0a6984157b65173e2e02f7f01a5ec533c447
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2013-10-19 21:01:14 +0900

    Add DbInterface

---
+++ src/Spika/Db/DbInterface.php
@@ -0,0 +1,27 @@
+<?php
+namespace Spika\Db;
+
+interface DbInterface
+{
+	public function unregistToken($userId);
+
+    public function checkEmailIsUnique($email);
+
+    public function checkUserNameIsUnique($name);
+
+    public function checkGroupNameIsUnique($name);
+
+    public function doSpikaAuth($requestBody);
+
+    public function saveUserToken($userJson, $id);
+
+    public function doPostRequest($requestBody);
+
+    public function doGetRequestGetHeader($queryString, $stripCredentials = true);
+
+    public function doGetRequest($queryString, $stripCredentials = true);
+
+    public function doPutRequest($id, $requestBody);
+
+    public function doDeleteRequest($id, $rev);
+}

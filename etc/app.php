967fbc25cb012a1932f199680df04f6d872cc346
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2013-10-19 18:24:06 +0900

    Move namespace

--- etc/app.php
+++ etc/app.php
@@ -17,16 +17,6 @@ require_once __DIR__.'/../vendor/autoload.php';
 require_once __DIR__.'/../init.php';
 require_once __DIR__.'/../etc/tokenCheker.php';
 require_once __DIR__.'/../services/SpikaDBProvider.php';
-require_once __DIR__.'/../controllers/GeneralAPIHandlerController.php';
-require_once __DIR__.'/../controllers/CheckUniqueController.php';
-require_once __DIR__.'/../controllers/AuthController.php';
-require_once __DIR__.'/../controllers/SignoutController.php';
-require_once __DIR__.'/../controllers/SearchUserController.php';
-require_once __DIR__.'/../controllers/SearchGroupController.php';
-require_once __DIR__.'/../controllers/FileController.php';
-require_once __DIR__.'/../controllers/ReportController.php';
-require_once __DIR__.'/../controllers/SendPasswordController.php';
-require_once __DIR__.'/../controllers/InstallerController.php';
 require_once __DIR__.'/../etc/utils.php';
 
 
@@ -53,13 +43,13 @@ $app->register(new Spika\SpikaDBProvider(), array(
 ));
 
 
-$app->mount('/', new Spika\InstallerController());
-$app->mount('/api/', new Spika\SendPasswordController());
-$app->mount('/api/', new Spika\ReportController());
-$app->mount('/api/', new Spika\FileController());
-$app->mount('/api/', new Spika\SearchGroupController());
-$app->mount('/api/', new Spika\SearchUserController());
-$app->mount('/api/', new Spika\SignoutController());
-$app->mount('/api/', new Spika\CheckUniqueController());
-$app->mount('/api/', new Spika\AuthController());
-$app->mount('/api/', new Spika\GeneralAPIHandlerController());
+$app->mount('/', new Spika\Controllers\InstallerController());
+$app->mount('/api/', new Spika\Controllers\SendPasswordController());
+$app->mount('/api/', new Spika\Controllers\ReportController());
+$app->mount('/api/', new Spika\Controllers\FileController());
+$app->mount('/api/', new Spika\Controllers\SearchGroupController());
+$app->mount('/api/', new Spika\Controllers\SearchUserController());
+$app->mount('/api/', new Spika\Controllers\SignoutController());
+$app->mount('/api/', new Spika\Controllers\CheckUniqueController());
+$app->mount('/api/', new Spika\Controllers\AuthController());
+$app->mount('/api/', new Spika\Controllers\GeneralAPIHandlerController());

84d5329cbdd737db172666a6a5e7a72969104ec3
Kuzuha SHINODA <kuzuha01@hotmail.com>
2013-10-19 13:46:53 +0900

    added phpunit sample

---
+++ tests/Spika/HelloTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Spika;
+
+class HelloTest extends \PHPUnit_Framework_TestCase {
+
+    public function testHello()
+    {
+        assertSame('hello', 'hello');
+    }
+
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,4 @@
+<?php
+
+require 'vendor/autoload.php';
+require 'PHPUnit/Framework/Assert/Functions.php';

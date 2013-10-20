0a498c49aad99b26f3bb310e746c3a5aea674ce6
Akihito Nakano <sora.akatsuki@gmail.com>
2013-10-20 15:44:08 +0900

    added ReportControllerTest

---
+++ tests/Spika/Controller/ReportControllerTest.php
@@ -0,0 +1,36 @@
+<?php
+namespace Spika\Controller;
+
+use Silex\Application;
+use Silex\WebTestCase;
+use Silex\ControllerProviderInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\ParameterBag;
+
+class ReportControllerTest extends WebTestCase
+{
+    public function createApplication()
+    {
+        require SPIKA_ROOT . '/etc/app.php';
+
+        $mailer = $this->getMockBuilder('\Silex\Provider\SwiftmailerServiceProvider')
+            ->setMethods(array('send'))
+            ->disableOriginalConstructor()
+            ->getMock();
+        $mailer->expects(once())
+            ->method('send')
+            ->with(isInstanceOf('Swift_Message'));
+        $app['mailer'] = $mailer;
+
+        return $app;
+    }
+
+    /** @test */
+    public function reportViolationSendsMailAndReturnsOK()
+    {
+        $client = $this->createClient();
+        $crawler = $client->request('GET', '/api/reportViolation.php', array('docment_id' => 'testid'));
+        assertSame(true, $client->getResponse()->isOk());
+        assertSame('OK', $client->getResponse()->getContent());
+    }
+}

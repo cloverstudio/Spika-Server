f09d509e84f2d1ed48047f13e6fb8ebbfaf0511c
Ken Yasue <ken.yasue@clover-studio.com>
2014-01-23 19:11:59 +0100

    adopt to EY working

--- deploy/after_bundle.rb
+++ deploy/after_bundle.rb
@@ -1,3 +1,3 @@
-run "chmod -R 777 #{shared_path}/logs"
-run "mkdir #{shared_path}/uploads"
-run "chmod -R 777 #{shared_path}/uploads"
+run "chmod -R 777 #{config.shared_path}/logs"
+run "mkdir #{config.shared_path}/uploads"
+run "chmod -R 777 #{config.shared_path}/uploads"

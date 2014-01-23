f8f9f375599a7c1d6b9604c1a23a5c7674dc1e29
Ken Yasue <ken.yasue@clover-studio.com>
2014-01-23 22:01:14 +0100

    working on EY support

--- deploy/after_bundle.rb
+++ deploy/after_bundle.rb
@@ -1,5 +1,5 @@
-run "ln -s /data/#{config.app}/shared/logs #{config.shared_path}/logs"
-run "chmod -R 777 /data/#{config.app}/shared/logs"
+run "ln -s /data/#{config.app}/shared/log #{config.current_path}/logs"
+run "chmod -R 777 /data/#{config.app}/shared/log"
 run "mkdir #{config.shared_path}/uploads"
 run "chmod -R 777 #{config.shared_path}/uploads"
-run "ln -s /data/#{config.app}/shared/uploads #{config.shared_path}/uploads"
+run "ln -s /data/#{config.app}/shared/uploads #{config.current_path}/uploads"

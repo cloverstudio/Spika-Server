9c7b6741dccb699a6b464c4922714ba66c682e4a
Ken Yasue <ken.yasue@clover-studio.com>
2014-01-23 22:06:44 +0100

    EA support

--- deploy/after_bundle.rb
+++ deploy/after_bundle.rb
@@ -1,5 +1,5 @@
-run "ln -s /data/#{config.app}/shared/log #{config.current_path}/logs"
-run "chmod -R 777 /data/#{config.app}/shared/log"
+run "ln -s #{config.shared_path}/log #{config.current_path}/logs"
+run "chmod -R 777 #{config.shared_path}/log"
 run "mkdir #{config.shared_path}/uploads"
 run "chmod -R 777 #{config.shared_path}/uploads"
-run "ln -s /data/#{config.app}/shared/uploads #{config.current_path}/uploads"
+run "ln -s #{config.shared_path}/uploads #{config.current_path}/uploads"
